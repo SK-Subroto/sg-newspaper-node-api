52fec6256377a9d3a603a030e37fc131bc154803
SK-Subroto <subroto35-1734@diu.edu.bd>
2022-03-09 20:25:57 +0600

    home url update

--- src/index.js
+++ src/index.js
@@ -38,33 +38,33 @@ app.listen(port, () => {
 })
 
 app.get('/', (req, res) => {
-    res.send(`
-        1. GET: http://localhost:5000/
-        2. GET: http://localhost:5000/api/users
-        3. POST: http://localhost:5000/api/users/registration
-        4. POST: http://localhost:5000/api/users/login
-        5. POST: http://localhost:5000/api/users/logout
-        6. POST: http://localhost:5000/api/users/logout-all
-        7. GET: http://localhost:5000/api/users
-        8. GET: http://localhost:5000/api/users/<id>
-        9. PATCH: http://localhost:5000/api/users/<id>
-        10. DELETE: http://localhost:5000/api/users/<id>
-        11. PATCH: http://localhost:5000/api/active-user/<id>
-        12. GET: http://localhost:5000/api/editors
-        13. GET: http://localhost:5000/api/editors/<id>
-        14. PATCH: http://localhost:5000/api/make-admin/<id>
-        15. PATCH: http://localhost:5000/api/admin-profile/me
-        16. GET: http://localhost:5000/api/admin-profile/me
-        17. GET: http://localhost:5000/api/search-editor/<email>
-        18. GET: http://localhost:5000/api/editor-profile/me
-        19. PATCH: http://localhost:5000/api/editor-profile/me
-        20. POST: http://localhost:5000/api/articles/
-        21. PATCH: http://localhost:5000/api/articles/<id>
-        22. GET: http://localhost:5000/api/articles/
-        23. GET: http://localhost:5000/api/articles/<id>
-        24. GET: http://localhost:5000/api/articles/<id>/image
-        25. PATCH: http://localhost:5000/api/articles/<id>/image
-        26. DELETE: http://localhost:5000/api/articles/<id>/image
-        27. DELETE: http://localhost:5000/api/articles/<id>
-    `);
+    res.json({
+        "1": "GET: http://localhost:5000/",
+        "2": "GET: http://localhost:5000/api/users",
+        "3": "POST: http://localhost:5000/api/users/registration",
+        "4": "POST: http://localhost:5000/api/users/login",
+        "5": "POST: http://localhost:5000/api/users/logout",
+        "6": "POST: http://localhost:5000/api/users/logout-all",
+        "7": "GET: http://localhost:5000/api/users",
+        "8": "GET: http://localhost:5000/api/users/<id>",
+        "9": "PATCH: http://localhost:5000/api/users/<id>",
+        "10": "DELETE: http://localhost:5000/api/users/<id>",
+        "11": "PATCH: http://localhost:5000/api/active-user/<id>",
+        "12": "GET: http://localhost:5000/api/editors",
+        "13": "GET: http://localhost:5000/api/editors/<id>",
+        "14": "PATCH: http://localhost:5000/api/make-admin/<id>",
+        "15": "PATCH: http://localhost:5000/api/admin-profile/me",
+        "16": "GET: http://localhost:5000/api/admin-profile/me",
+        "17": "GET: http://localhost:5000/api/search-editor/<email>",
+        "18": "GET: http://localhost:5000/api/editor-profile/me",
+        "19": "PATCH: http://localhost:5000/api/editor-profile/me",
+        "20": "POST: http://localhost:5000/api/articles/",
+        "21": "PATCH: http://localhost:5000/api/articles/<id>",
+        "22": "GET: http://localhost:5000/api/articles/",
+        "23": "GET: http://localhost:5000/api/articles/<id>",
+        "24": "GET: http://localhost:5000/api/articles/<id>/image",
+        "25": "PATCH: http://localhost:5000/api/articles/<id>/image",
+        "26": "DELETE: http://localhost:5000/api/articles/<id>/image",
+        "27": "DELETE: http://localhost:5000/api/articles/<id>",
+    });
 });
